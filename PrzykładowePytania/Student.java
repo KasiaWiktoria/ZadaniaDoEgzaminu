8a80d481fda810ecac2b5a90c60f6788dae502a6
KasiaWiktoria <48280406+KasiaWiktoria@users.noreply.github.com>
2019-06-15 19:31:32 +0200

    zastosowanie systemowej funkcji max

--- PrzykładowePytania/Student.java
+++ PrzykładowePytania/Student.java
@@ -14,9 +14,9 @@ public class Student {
     }
 
     public void dodajOcene(String data, double ocena){
-        if (ocena != 2 || ocena != 2.5 || ocena != 3|| ocena != 3.5 || ocena != 4 || ocena != 4.5 || ocena != 5 )
+        if (ocena != 2 && ocena != 2.5 && ocena != 3 && ocena != 3.5 && ocena != 4 && ocena != 4.5 && ocena != 5 )
             throw new IllegalArgumentException("Próbujesz wpisać ocenę poza skalą");
-        
+
         listaOcen.put(data, ocena);
     }
 
@@ -36,28 +36,15 @@ public class Student {
     }
 
     public double najlepszaOcena(){
+        return Collections.max(listaOcen.values());
+        /*
+        //Ewentualnie:
         double max = 0;
         for (double ocena : listaOcen.values()) {
             if(ocena > max)
                 max = ocena;
         }
         return max;
+        */
     }
-
-    public static void main(String [] args){
-
-        Student A = new Student("Adam", "Nowak", 123456);
-        Random gen = new Random();
-        for (int i = 0; i < 9; i++) {
-            double o = gen.nextDouble() % 5 + 1;
-            System.out.print(o + "\n");
-            A.dodajOcene("0" + i + "-06-2019",o);
-        }
-        System.out.print(A.toString() + " lista ocen: ");
-        for(double ocena : A.listaOcen.values()){
-            System.out.print( ocena + "\n");
-        }
-        System.out.print(A.listaOcen.values());
-    }
-
 }
