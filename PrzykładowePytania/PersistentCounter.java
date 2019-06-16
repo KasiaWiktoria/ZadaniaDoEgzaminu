f35822af4e5406039ea44ce7d7b154c36d557f61
KasiaWiktoria <48280406+KasiaWiktoria@users.noreply.github.com>
2019-06-16 16:50:12 +0200

    nowe

---
+++ PrzykładowePytania/PersistentCounter.java
@@ -0,0 +1,25 @@
+public class PersistentCounter extends Thread {
+    private long counter = 0;
+    private long interval;
+
+    public PersistentCounter(long counter, long interval){
+        this.counter = counter;
+        this.interval = interval;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while(true) {
+                Thread.sleep((int)interval);
+                this.counter++;
+            }
+        }catch (InterruptedException e){
+            this.counter = 0;
+        }
+    }
+
+    public long getCounter(){
+        return counter;
+    }
+}
